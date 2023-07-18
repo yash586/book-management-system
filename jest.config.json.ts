2cbfd92c55e85dd6ea9ec40c4a856ebefd80d6a7
yash586 <55210026+yash586@users.noreply.github.com>
2023-07-18 20:05:14 +0100

    clear git cache

--- jest.config.json.ts
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  transform: {
-    "^.+\\.(ts|tsx)$": "babel-jest",
-  },
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-};
--- src/routes/book.route.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import bookController from "../controller/book.controller";
-
-const router = express.Router();
-
-router.get("/books", bookController.getBooks);
-
-export default router;
